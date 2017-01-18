7fd37f575f6a73d41fb574e03196c113f6a0ef79
Johan Dahl <johan.dalabacka@gmail.com>
2017-01-19 00:06:35 +0100

    Use integer arithmetics instead

--- doubler.go
+++ doubler.go
@@ -2,106 +2,39 @@ package main
 
 import (
 	"fmt"
-	//"reflect"
 	"os"
-	"strconv"
-	"strings"
 )
 
-func countIntegers(arr []string) int {
-	return len(arr)
-}
-
-// moves last ingteger to first position in an array
-func moveLastToFirst(number int) int {
-	str := strconv.Itoa(number)
-
-	// string array to store each number
-	arr := []string{}
-
-	// loop thru string, add each number to slice
-	for _, x := range str {
-		number := string(x)
-		arr = append(arr, number)
-	}
-
-	fmt.Println(countIntegers(arr))
-	fmt.Println("\n")
-
-	// get last # of array
-	lastNum := arr[len(arr)-1]
-
-	// move last number to 1st position
-	arr = append([]string{lastNum}, arr...)
-
-	// get index of last element
-	lastindex := len(arr) - 1
-
-	// remove last number from array (duplicate)
-	arr = append(arr[:lastindex])
-
-	// join array elements into single number
-	arrString := strings.Join(arr[:], "")
-
-	// convert string array to integer
-	num, _ := strconv.Atoi(arrString)
-
-	return num
-}
-
-func compare(i int) string {
-
-	double := i * 2
-	ltf := moveLastToFirst(i)
-
-	if double == ltf {
-
-		return "found"
-	}
-
-	return "continue"
-}
-
 func main() {
-
-	i := 1
+	var i uint64 = 1
+	var e uint64 = 1
+	var lim uint64 = 10
+	// Eg if i = 123 is e = 100 and lim = 1000
+	// the last = 3 and d = 12 + 3 * 100 = 312
 
 	for {
-
 		fmt.Println(i)
-		if compare(i) == "found" {
-			fmt.Println("FOUND")
+		last := i % 10 // Get last digit
+		if last != 0 {
+			d := i/10 + last*e // but last + last * e
+			// If d is double of i are the ready
+			if i*2 == d {
+				fmt.Println("FOUND")
 
-			file, err := os.Create("/tmp/result.txt")
-			if err != nil {
-				panic(err)
+				file, err := os.Create("/tmp/result.txt")
+				if err != nil {
+					panic(err)
+				}
+				fmt.Fprintln(file, i)
+				defer file.Close()
+				return
 			}
-
-			fmt.Fprintln(file, i)
-
-			defer file.Close()
-
-			break
-
-		} else {
-			i += 1
+		}
+		i++
+		// If n get over lim make e and lim 10 times bigger
+		if i >= lim {
+			lim *= 10
+			e *= 10
 		}
 	}
-
-	// fmt.Println(arrString)
-	// fmt.Println(reflect.TypeOf(num)) // get var type
-
-	//	num := strconv.Atoi(arrString)
-	//
-	// fmt.Println("last number", numSlice[lastNum])
-
-	// fmt.Println(numSlice)
-	// lastNum := numSlice[len(numSlice)-1]
-	// numSlice[0] = lastNum
-	// numSlice = append(numSlice, lastNum)
-	// fmt.Println(lastNum)
-	// fmt.Println(numSlice)
-
-	//strArr := strings.Fields(str)
-
 }
